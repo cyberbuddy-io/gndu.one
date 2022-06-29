c3912467f16eaa1a4261415357be7d0bbabef420
Rakshit Mehra <rakshitmehra88@gmail.com>
2022-06-29 20:30:42 +0530

    DGSS-Website

---
+++ Datagenics-Student-Society/js/custom.js
@@ -0,0 +1,31 @@
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+//  course section owl carousel
+
+$(".course_owl-carousel").owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 5,
+    autoHeight: true,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    autoWidth: true
+});
+
+
+/** google_map js **/
+
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(40.712775, -74.005973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
